72296394f6283fc405e3571f0d3811216d63a3e3
Copy <71971770+Copy05@users.noreply.github.com>
2023-04-21 12:10:56 +0200

    errors corrected

--- block.py
+++ block.py
@@ -4,16 +4,17 @@ blockedUsers = list(set(user['screen_name'] for user in api.request('blocks/list
 
 with open('Twitter.txt', 'r') as file:
     users = file.readlines()
+    users = [i[1:] for i in users]
     print(len(users))
     for i in range(len(users)):
         for user in list(set(post['user']['screen_name'] for post in api.request('search/tweets', {'q': str(i), 'count':'100', 'include_entities':'false'}))):
             try:
                 if user in blockedUsers:
-                    print(f"user: {i} is already blocked")
+                    print(f"user: {users[i]} is already blocked")
                     continue
                 else:
-                    block = api.request('blocks/create', {'screen_name': i})
+                    block = api.request('blocks/create', {'screen_name': users[i]})
                 if block:
-                    print(f"user: {i} has been blocked")
+                    print(f"user: {users[i]} has been blocked")
             except Exception:
                 print("Something went wrong")
